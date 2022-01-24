c1c0d7994823b5927ef1ee441ef998f656037905
Shinnosuke-Takagi <s.takagi5664@gmail.com>
2022-01-24 12:07:13 +0900

    Fix api route

--- src/routes/api.php
+++ src/routes/api.php
@@ -20,10 +20,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/folders', [FolderController::class, 'index']);
+Route::get('/folder/list', [FolderController::class, 'index']);
 Route::get('/folder/{folder}', [FolderController::class, 'show']);
-Route::get('/author/{author}/folders', [FolderController::class, 'foldersByUserId']);
+Route::get('/folder/author/{author}/list', [FolderController::class, 'foldersByUserId']);
 
-Route::get('/tasks', [TaskController::class, 'index']);
+Route::get('/task/list', [TaskController::class, 'index']);
 Route::get('/task/{task}', [TaskController::class, 'show']);
-Route::get('/folder/{belongsFolder}/tasks', [TaskController::class, 'tasksByFolderId']);
+Route::get('/task/folder/{belongsFolder}/list', [TaskController::class, 'tasksByFolderId']);
